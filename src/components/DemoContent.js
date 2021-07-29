4308b23903e32ef92d6c30729bb649c918ee5f05
Statikman619 <martinez.michael02@gmail.com>
2021-07-28 19:36:45 -0700

    started the demoContent

--- src/components/DemoContent.js
+++ src/components/DemoContent.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-function DemoContent() {
-  return <h1>DemoContent</h1>;
-}
-
-export default DemoContent;
+function DemoContent(props) {
+  // Creating static properties for an iframe element or img element depending on the format of the demo content
+  const iframe = (
+    <iframe
+      width="768"
+      height="432"
+      title={props.name}
+      src={props.content}
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    ></iframe>
+  );
+  const img = (
+    <img src={props.content} height="100%" width="100%" alt={props.name}></img>
+  );
