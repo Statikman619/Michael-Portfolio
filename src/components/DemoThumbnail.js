03a01dcdf3a94f57897f01adec7bc254c0d405c8
Statikman619 <martinez.michael02@gmail.com>
2021-07-28 19:35:51 -0700

    started my demo

--- src/components/DemoThumbnail.js
+++ src/components/DemoThumbnail.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-function DemoThumbnail() {
-  return <h1>DemoThumbnail</h1>;
+// This renders the thumbnail image for each project that will in turn launch the modal
+function DemoThumbnail(props) {
+  return (
+    <div>
+      <img
+        className="demo-thumbnail"
+        src={props.thumbnail}
+        alt={props.name}
+        onClick={props.toggle}
+      />
+    </div>
+  );
 }
 
 export default DemoThumbnail;
--- src/components/ProfileEmbed.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ProfileEmbed() {
-  return <h1>ProfileEmbed</h1>;
-}
-
-export default ProfileEmbed;
