38096c178101068328603657165370b3c5b6b916
Statikman619 <martinez.michael02@gmail.com>
2021-07-30 18:52:35 -0700

    finished my password generator info

--- src/project.js
+++ src/project.js
@@ -10,56 +10,56 @@ const projects = [
     id: 6,
     name: "day-planner",
     projectLinks: [
-        {type: "Repository", url: "https://github.com/Statikman619/Work-Day-Scheduler" }
+        {type: "Repository", url: "https://github.com/Statikman619/Work-Day-Scheduler" },
         {type: "Deployment", url: "https://statikman619.github.io/Work-Day-Scheduler/" }
     ]
-    skillsUsed: "HTML, CSS, JQuery and Moment.js"
+    skillsUsed: "HTML, CSS, JQuery and Moment.js",
     demoThumbnail: [dayPlannerImg],
     demoContent: [dayPlannerGif],
     demoType: "gif",
-    summary: "Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code."
+    summary: "Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code.",
     details: "An application that allows users to add important events to a daily planner. So that they can manage their time effectively."
   },
   {
       id: 5,
-      name: "movie-buffs"
+      name: "movie-buffs",
       projectLinks: [
-          {type : "Repository", url "https://github.com/JG-77/Movie-Buffs-Group-Project"}
+          {type : "Repository", url "https://github.com/JG-77/Movie-Buffs-Group-Project"},
           {type : "Deployment", url "https://jg-77.github.io/Movie-Buffs-Group-Project/"}
       ]
-      skillsUsed: ""
+      skillsUsed: "",
       demoThumbnail: [movieBuffsImg],
       demoContent: [movieBuffsGif],
-      demoType: "gif"
-      summary: ""
+      demoType: "gif",
+      summary: "",
       details ""
   }
   {
     id: 4,
-    name: "note-taker"
+    name: "note-taker",
     projectLinks: [
-        {type : "Repository", url "https://github.com/Statikman619/Note-Taker"}
+        {type : "Repository", url "https://github.com/Statikman619/Note-Taker"},
         {type : "Deployment", url "https://serene-ridge-18121.herokuapp.com/"}
     ]
-    skillsUsed: "HTML, CSS, JS, Express.js, JSON and UUID"
+    skillsUsed: "HTML, CSS, JS, Express.js, JSON and UUID",
     demoThumbnail: [noteTakerImg],
     demoContent: [noteTakerGif],
-    demoType: "gif"
-    summary: "It's your job to build the back end, connect the two, and then deploy the entire application to Heroku."
+    demoType: "gif",
+    summary: "It's your job to build the back end, connect the two, and then deploy the entire application to Heroku.",
     details "An application called Note Taker that can be used to write and save notes."
 }
 {
     id: 3,
-    name: "open-weather-map"
+    name: "open-weather-map",
     projectLinks: [
-        {type : "Repository", url "https://github.com/Statikman619/Open-Weather-Map/tree/master"}
+        {type : "Repository", url "https://github.com/Statikman619/Open-Weather-Map/tree/master"},
         {type : "Deployment", url "https://statikman619.github.io/Open-Weather-Map/"}
     ]
-    skillsUsed: "HTML, CSS, JS, LocalStorage and Open Weather Map API"
+    skillsUsed: "HTML, CSS, JS, LocalStorage and Open Weather Map API",
     demoThumbnail: [openWeatherMapImg],
     demoContent: [openWeatherMapGif],
-    demoType: "gif"
-    summary: "Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS."
+    demoType: "gif",
+    summary: "Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.",
     details "An application called Open Weather Map that allows you to get current weather data from various cities"
 }
 {
@@ -69,12 +69,12 @@ const projects = [
         {type : "Repository", url "https://github.com/Statikman619/Password-Generator"}
         {type : "Deployment", url "https://statikman619.github.io/Password-Generator/"}
     ]
-    skillsUsed: "HTML, CSS, JS, "
+    skillsUsed: "HTML, CSS, JS, ",
     demoThumbnail: [passwordGeneratorImg],
     demoContent: [passwordGeneratorGif],
     demoType: "gif"
-    summary: ""
-    details ""
+    summary: "For this project we had to modify starter code to make it have a clean, polished and responsive look.",
+    details "An application that enables employees to generate random passwords based on criteria that they’ve selected. "
 }
 {
     id: 1,
@@ -83,7 +83,7 @@ const projects = [
         {type : "Repository", url "https://github.com/Statikman619/Workout-Tracker"}
         {type : "Deployment", url "https://mikes-workout-tracker.herokuapp.com/?id=60f798454d65b0001558decd"}
     ]
-    skillsUsed: "HTML, CSS, JS, "
+    skillsUsed: "HTML, CSS and JS",
     demoThumbnail: [workoutTrackerImg],
     demoContent: [workoutTrackerGif],
     demoType: "gif"
