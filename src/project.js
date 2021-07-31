56490379ac1132e8552dad80783ce20c2e1ecbc3
Statikman619 <martinez.michael02@gmail.com>
2021-07-30 17:32:07 -0700

    started working on my project.js

---
+++ src/project.js
@@ -0,0 +1,6 @@
+import dayPlannerImg from "./assets/demo-thumbnails/day-planner.png";
+import movieBuffsImg from "./assets/demo-thumbnails/movie-buffs.png";
+import noteTakerImg from "./assets/demo-thumbnails/note-taker.png";
+import openWeatherMapImg from "./assets/demo-thumbnails/open-weather-map.png";
+import passwordGeneratorImg from "./assets/demo-thumbnails/password-generator.png";
+import workoutTrackerImg from "./assets/demo-thumbnails/workout-tracker.png";
