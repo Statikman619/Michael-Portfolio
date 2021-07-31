7c98080507d3c899b9af7f10c47b41e2f70b2bb3
Statikman619 <martinez.michael02@gmail.com>
2021-07-30 17:59:35 -0700

    created the project dayplanner

--- src/project.js
+++ src/project.js
@@ -4,3 +4,21 @@ import noteTakerImg from "./assets/demo-thumbnails/note-taker.png";
 import openWeatherMapImg from "./assets/demo-thumbnails/open-weather-map.png";
 import passwordGeneratorImg from "./assets/demo-thumbnails/password-generator.png";
 import workoutTrackerImg from "./assets/demo-thumbnails/workout-tracker.png";
+
+const projects = [
+  {
+    id: 6,
+    name: "day-planner",
+    projectLinks: [
+        {type: "Repository", url: "https://github.com/Statikman619/Work-Day-Scheduler" }
+        {type: "Deployment", url: "https://statikman619.github.io/Work-Day-Scheduler/" }
+    ]
+    skillsUsed: "HTML, CSS, JQuery and Moment.js"
+    demoThumbnail: [dayPlannerImg],
+    demoContent: [dayPlannerGif],
+    demoType: "gif",
+    summary: "Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code."
+    details: "An application that allows users to add important events to a daily planner. So that they can manage their time effectively."
+  },
+];
+export default projects;
