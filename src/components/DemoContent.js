6b3cfacdc9da8af01fbabe5e916809035ce8f8ce
Statikman619 <martinez.michael02@gmail.com>
2021-08-02 22:09:51 -0700

    fixed the issue that made my box disappear

--- src/components/DemoContent.js
+++ src/components/DemoContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import DemoThumbnail from "../DemoThumbnail";
+import DemoThumbnail from "./DemoThumbnail";
 
 function DemoContent(props) {
   // Creating static properties for an stills element or img element depending on the format of the demo content
@@ -31,48 +31,48 @@ function DemoContent(props) {
 
   return (
     // inline style rule determining when the modal opens and closes
-    <div className="demo-window" style={{ display: props.display }}>
-      <button className="btn-link close">
-        {/* function to close the modal is passed as a prop below */}
-        <i className="fa fa-window-close" onClick={props.toggle}></i>
-      </button>
-      <div className="demo-content">{thingToDisplay}</div>
-      <section className="row section-row bottom-row">
-        <div className="col-md-3 col-folio">
-          <h3>
-            <strong>{project.name}</strong>
-          </h3>
-          <p>
-            <strong>Project Links</strong>
-            {/* Rendering each project link within the ProjectLinks component */}
-            {/* {props.links.map((link) => {
+    // <div className="demo-window">
+    //   <button className="btn-link close">
+    //     {/* function to close the modal is passed as a prop below */}
+    //     <i className="fa fa-window-close" onClick={props.toggle}></i>
+    //   </button>
+    //   <div className="demo-content">{thingToDisplay}</div>
+    <section className="row section-row bottom-row">
+      <div className="col-md-3 col-folio">
+        <h3>
+          <strong>{project.name}</strong>
+        </h3>
+        <p>
+          <strong>Project Links</strong>
+          {/* Rendering each project link within the ProjectLinks component */}
+          {/* {props.links.map((link) => {
               return <ProjectLinks link={link} />;
             })} */}
-          </p>
-          <p>
-            <strong>Skills Used |</strong> {project.skills}
-          </p>
+        </p>
+        <p>
+          <strong>Skills Used |</strong> {project.skills}
+        </p>
 
-          <p>
-            <strong>Demos (click to enlarge)</strong>
-          </p>
+        <p>
+          <strong>Demos (click to enlarge)</strong>
+        </p>
 
-          <DemoThumbnail
-            thumbnail={project.thumbnail}
-            name={project.name}
-            // toggle={project.toggle}
-          />
-        </div>
-        <div className="col-md-4">
-          <h3>Project Summary</h3>
-          <p>{project.summary}</p>
-        </div>
-        <div className="col-md-5">
-          <h3>Project Details</h3>
-          <p>{project.details}</p>
-        </div>
-      </section>
-    </div>
+        <DemoThumbnail
+          thumbnail={project.thumbnail}
+          name={project.name}
+          // toggle={project.toggle}
+        />
+      </div>
+      <div className="col-md-4">
+        <h3>Project Summary</h3>
+        <p>{project.summary}</p>
+      </div>
+      <div className="col-md-5">
+        <h3>Project Details</h3>
+        <p>{project.details}</p>
+      </div>
+    </section>
+    // </div>
   );
 }
 
